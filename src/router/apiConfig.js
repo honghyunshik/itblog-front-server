f9fca468797723438ee6dce62f9eee3e92cf3cfb
hyunshik <hshong0907@gmail.com>
2024-01-26 14:45:28 +0900

    feat: 토큰을 동반하는 요청 객체 instanceWithToken 생성

--- src/router/apiConfig.js
+++ src/router/apiConfig.js
@@ -9,4 +9,22 @@ const instance = axios.create({
     },
 })
 
-export { instance }
+const instanceWithToken = axios.create({
+  baseURL: process.env.VUE_APP_API_URL,
+  headers: {
+      'Content-Type': 'application/json',
+    },
+})
+
+//인터셉터로 최신 엑세스 토큰 추적
+instanceWithToken.interceptors.request.use(function (config) {
+  const token = sessionStorage.getItem('accessToken');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, function (error) {
+  return Promise.reject(error);
+});
+
+export { instance, instanceWithToken }
